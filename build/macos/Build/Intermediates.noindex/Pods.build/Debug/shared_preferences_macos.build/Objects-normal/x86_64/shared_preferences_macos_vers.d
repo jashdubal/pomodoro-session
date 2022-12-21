61970ba153e50f51eaed0c2f5bf1e44da6009e36
Jash Dubal <73313777+jashdubal@users.noreply.github.com>
2022-12-20 18:50:18 -0700

    upload

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/Objects-normal/x86_64/shared_preferences_macos_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jashdubal/Desktop/dev/ht/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/DerivedSources/shared_preferences_macos_vers.c
