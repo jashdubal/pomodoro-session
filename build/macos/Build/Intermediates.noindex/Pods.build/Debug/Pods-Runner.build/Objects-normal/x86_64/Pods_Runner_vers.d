5edeb5fdf76a4e7df6813449220d5459609ae2db
Jash Dubal <73313777+jashdubal@users.noreply.github.com>
2022-12-20 18:44:31 -0700

    Delete build directory

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jashdubal/Desktop/dev/ht/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/Objects-normal/x86_64/shared_preferences_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/jashdubal/Desktop/dev/ht/macos/Pods/Target\ Support\ Files/shared_preferences_macos/shared_preferences_macos-dummy.m \
-  /Users/jashdubal/Desktop/dev/ht/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/module.modulemap \
-  /Users/jashdubal/Desktop/dev/ht/macos/Pods/Target\ Support\ Files/shared_preferences_macos/shared_preferences_macos-prefix.pch
